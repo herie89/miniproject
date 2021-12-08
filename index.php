f121da567a2db1bfae1e6900c573a000bc6c2251
herie89 <93757039+herie89@users.noreply.github.com>
2021-12-08 23:07:47 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="asset/css/style.css">
+
+    <title>Tokopedia</title>
+  </head>
+  <body>
+    <!-- header -->
+    <div class="header">
+
+      <div class="item-header-1 d-flex justify-content-between align-items-center">
+
+        <div class="d-flex align-items-center">
+          <img class="mr-2" src="asset/img/1.png" alt="">
+          <span>Download Tokopedia App</span>
+        </div>
+
+        <div class="d-flex">
+          <div class="mr-4">Tentang Tokopedia</div>
+          <div class="mr-4">Mitra Tokopedia</div>
+          <div class="mr-4">Mulai Berjualan</div>
+          <div class="mr-4">Promo Tokopedia</div>
+          <div class="mr-4">Tokopedia Care</div>
+        </div>
+
+      </div>
+
+      <div class="item-header-2 d-flex flex-column">
+        <div class="d-flex justify-content-between align-items-center">
+          <img class="img-tokopedia" src="asset/img/2.png" alt="">
+          <span class="mx-2 text-category">Kategory</span>
+          <div class="wrap-search">
+            <input type="text" class="form-control" placeholder="Cari Barang">
+            <div class="wrap-icon-search">
+              <img class="img-search" src="asset/img/3.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- end header -->
+
+
+
+
+
+
+
+
+
+
+   
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+  
+  </body>
+</html>
